5bf365adcbd066210e24f13e84933b563d7a1c41
enji-coder <47570231+enji-coder@users.noreply.github.com>
2024-01-02 22:41:48 +0530

    updates

--- TASK/Module 3 React Js/taskapp/src/App.jsx
+++ TASK/Module 3 React Js/taskapp/src/App.jsx
@@ -1,6 +1,6 @@
 // import Task1 from "./PracticalTask/task1";
 
-import Task1 from "./PracticalTask/task1";
+import Task1 from "./PracticalTask/Task1";
 import Task2 from "./PracticalTask/task2";
 
 export default function App(){
